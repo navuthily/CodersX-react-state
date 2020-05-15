c28eb2bdd5ba435ad6781be859cba0d630e6fe16
lyna <vuthilyna304@gmail.com>
2020-05-15 16:46:43 +0700

    add search box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import './App.css';
-import  SearchBox from './components/SearchBox';
+import React, { Component } from "react";
+import './App.css'
+import SearchBox from "../src/components/SearchBox ";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 class App extends Component {
-
   state = {
     focus: false
   };
@@ -16,17 +15,20 @@ class App extends Component {
   handleInputBlur = () => {
     this.setState({ focus: false });
   };
+
   render() {
     return (
-    <div>
-     <div>
+      <div className="ss_header">
+           <div class="box">
+          <div class="container-1">
         <SearchBox
           onFocus={this.handleInputFocus}
           onBlur={this.handleInputBlur}
         />
-        {this.state.focus ? <button class="icon"><i class="fa fa-search"></i></button> : null}          
-      </div>  
-    </div>
+         {this.state.focus ? <button class="icon"><i class="fa fa-search"></i></button> : null}  
+      </div>
+      </div>
+      </div>
     );
   }
 }
