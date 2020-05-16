0501b570d55b94c25fb6414b4dd22ddd4076e426
lyna <vuthilyna304@gmail.com>
2020-05-16 18:29:43 +0700

    add search box

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from "react";
 import './App.css'
 import SearchBox from "../src/components/SearchBox ";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Modal, Button} from 'react-bootstrap'
 import "font-awesome/css/font-awesome.min.css";
 class App extends Component {
+  // constructor(){
+  //   super()
+  //   this.state={
+  //     show:false
+  //   }
+  // }
   state = {
     focus: false,
-    limit:false
+    limit:false,
+    show: false
   };
   handleInputFocus = () => {
     this.setState({ focus: true });
@@ -25,9 +33,13 @@ class App extends Component {
     }
   };
   
+  handleModal=()=>{
+    this.setState({show:!this.state.show})
+  }
   render() {
     return (
-      <div className="ss_header">
+<div>
+    <div className="ss_header">
          <div class="box">
           <div class="container-1">
           <SearchBox
@@ -38,9 +50,19 @@ class App extends Component {
            className= {this.state.limit ? 'border-red' :''}
           />
          {this.state.focus ? <button class="icon"><i class="fa fa-search"></i></button> : null}  
+        </div>
        </div>
-       </div>
-      </div>
+    </div>
+  <Button onClick={this.handleModal}>open modal</Button>
+  <Modal show={this.state.show}>
+     <Modal.Header>Header</Modal.Header>
+     <Modal.Body>Body</Modal.Body>
+     <Modal.Footer>
+       <Button onClick={this.handleModal}>close modal</Button>
+     </Modal.Footer>
+  </Modal>
+
+</div>
     );
   }
 }
